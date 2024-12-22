c8731061f63a194f58ab3685225576c73569370c
Satii775 <124398902+Satii775@users.noreply.github.com>
2024-12-22 00:23:20 -0700

    st

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const isGitHubPages = process.env.NODE_ENV === "production";
+
+const assetPrefix = isGitHubPages ? "/satii775.github.io/" : "";
+const basePath = isGitHubPages ? "/satii775.github.io" : "";
+
+module.exports = {
+  output: "export",
+  assetPrefix,
+  basePath,
+};
