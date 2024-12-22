cfb65c5c82195c190bd7e7cb7084ca896a1f6078
Satii775 <124398902+Satii775@users.noreply.github.com>
2024-12-22 00:26:17 -0700

    arst

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
-
 const isGitHubPages = process.env.NODE_ENV === "production";
 
 const assetPrefix = isGitHubPages ? "/satii775.github.io/" : "";
 const basePath = isGitHubPages ? "/satii775.github.io" : "";
 
-module.exports = {
-  output: "export",
-  assetPrefix,
-  basePath,
+export default {
+    nextConfig, 
+    output: "export",
+    assetPrefix,
+    basePath,
 };
